ecc0775d55480f91bce2adcd6f4b9a9ea8e037cc
s20002 <s20002@std.it-college .ac.jp>
2020-06-24 16:22:02 +0900

    ファイルパターンの指定の追加

--- findgrep.sh
+++ findgrep.sh
@@ -2,8 +2,12 @@
 
 pattern=$1
 directory=$2
-find . -type f | xargs grep -nH "$pattern"
+name=$3
+
+#find . -type f | xargs grep -nH "$pattern"
 if [ -z "$directory" ]; then
 	directory='.'
 fi
+if [ -z "$name" ]; then
+	name='*'
 find "$direcrory" -type f -print0 | xargs -0 grep -nH "$pattern"
