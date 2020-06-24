13fb5586f35be0109adc2e15e24fa286b0b5ee72
s20002 <s20002@std.it-college .ac.jp>
2020-06-24 14:48:03 +0900

    ファイルを探すディレクトリを指定できるようにする
    ディレクトリを移動しなくても任意の場所からファイルを探せるようにするため

--- findgrep.sh
+++ findgrep.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 pattern=$1
+directory=$2
 find . -type f | xargs grep -nH "$pattern"
+if [ -z "$directory" ]; then
+	directory='.'
+fi
+find "$direcrory" -type f -print0 | xargs -0 grep -nH "$pattern"
